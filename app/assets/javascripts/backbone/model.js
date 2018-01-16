746e4991aa7ff385bb01c8d66daba01eaaec463f
CJOShopping <cjoshopping@CJOShoppingui-iMac.local>
2018-01-16 14:35:12 +0900

    html빼고 backbone

--- app/assets/javascripts/backbone/model.js
+++ app/assets/javascripts/backbone/model.js
@@ -1,28 +1,36 @@
-//definitions
-var MyModel = Backbone.Model.extend({});
-var MyCollection = Backbone.Collection.extend({
-    model: MyModel
-});
-
-//wherever you need a collection instance
-collection = new MyCollection();
-
-//wherever you need to do the ajax
-var codee = code2;
-console.log(codee);
-Backbone.ajax({
-  type: "GET", 
-	dataType: "jsonp",
-	url: "http://display.cjmall.com/c/rest/item/"+codee+"/itemInfo.json?channelCode=30001001&isEmployee=false&isMyzone=false&isSimple=false&isSearch=false",
-	data: {},
-	xhrFields: {
-		withCredentials: true
-	},
-
-  success: function(val){
-    collection.add(val.result);
-    // this.collection.fetch();
-    console.log(collection.at(0));
-    // callback(collection);
+var MyModel = Backbone.Model.extend({
+  initialize: function(){
+    // console.log('aa');
+    var s = this;
+    s.url = "http://display.cjmall.com/c/rest/item/"+code2+"/itemInfo.json?channelCode=30001001&isEmployee=false&isMyzone=false&isSimple=false&isSearch=false";
+  },
+  
+  update: function(){
+    var s = this;
+    Backbone.ajax({
+      type: "GET", 
+      dataType: "jsonp",
+      url: s.url,
+      data: {},
+      xhrFields: {
+        withCredentials: true
+      },
+      success: function(val){
+        if(val.code==1 && val.status==200){
+          // console.log(val.result);
+          s.setData(val.result);
+          s.trigger('success');
+        }
+      }
+    });
+  },
+  setData: function(result){
+    var s = this;
+    // console.log(result);
+    s.set ({
+      imageURL  :result.imagesInfo.itemImages[0],
+      name      :result.detailInfo.dispItemName,
+      price     :result.detailInfo.clpSlPrc
+    });
   }
 });
--- app/assets/javascripts/backbone/view.js
+++ app/assets/javascripts/backbone/view.js
@@ -1,35 +1,23 @@
 var View = Backbone.View.extend({
-    el: '#container',
-    template: _.template("<h3>Hello <%=who%></h3>"),
     initialize: function(){
-            this.render();
-          },
+        var s = this;
+        var model = new MyModel();
+        model.update();
+        
+        s.model = model;
+        
+        s.listenTo(s.model, 'success', s.render);
 
-          render: function(){
-            // render the function using substituting the varible 'who' for 'world!'.
-            this.$el.html(this.template({who: 'dd'}));
-            // console.log(collection.models[0].attributes);
-            //***Try putting your name instead of world.
-          }
-    // el : element
-    // el: '#cc',
-    // events: {
-    //     "click button": "render"
-    // },
-    // render: function() {
-    //     var data = collection.at(0).attributes.result;
-    //     console.log(data.imagesInfo.itemImages);
-    //     console.log(data.detailInfo.dispItemName);
-    //     console.log(data.detailInfo.clpSlPrc);
-    //     // 이미지 경로 a.imagesInfo.itemImages
-    //     // 상품이름  a.detailInfo.dispItemName
-    //     //가격 a.detailInfo.clpSlPrc
-    // }
+
+        // model.on("success", movieSuccessHandler);
+        // this.listenTo(model,'success',this.render());  
+        // //view.listenTo(model, 'change', view.render);
+        //model.listenTo(model,'updated',this.render);
+    },
+    render: function(){
+        var s = this;
+        console.log(s.model);
+
+        // (this.el).html.append("<p>Hello World</p>");
+    }
 });
-var view = new View({ model: collection });
-// http://mobicon.tistory.com/240
-// https://stackoverflow.com/questions/9350726/raising-a-backbone-js-view-event(trigger)
-// http://webframeworks.kr/tutorials/backbonejs/backbone_events/
-// http://adrianmejia.com/blog/2012/09/11/backbone-dot-js-for-absolute-beginners-getting-started/
-
-// http://codebeerstartups.com/2012/12/9-collection-views-in-backbone-js-learning-backbone-js/
